053236e7838d3f6e7646316e84e30aec8006376f
Matteo Porrani <m.porrani@gmail.com>
2025-03-15 18:10:02 +0100

    fix type problem for moment import

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+/**
+ * The new import syntax imports all exports from the moment module and assigns them to the 'moment' namespace
+ * This matches the type expectations of the extendMoment function from moment-range
+ */
+import * as moment from "moment";
+import { extendMoment } from "moment-range";
+
+extendMoment(moment);
+moment.locale("fr");
+
+const app = createApp(App)
+app.config.globalProperties.moment = moment;
+
+
+app.use(store).use(router).mount('#app')
