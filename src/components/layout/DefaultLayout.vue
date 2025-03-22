99d4e9acbf5e3ad0cdef8bb718a380bb464c0460
Matteo Porrani <m.porrani@gmail.com>
2025-03-22 11:31:55 +0100

    add TW

--- src/components/layout/DefaultLayout.vue
+++ src/components/layout/DefaultLayout.vue
@@ -1,11 +1,13 @@
 <template>
-	<section class="default-layout-root">
-		<aside class="layout-menu">
+	<section
+		class="default-layout-root grid grid-cols-6 gap-2"
+	>
+		<aside class="layout-menu bg-stone-800 rounded p-2">
 			<slot name="menu">
 				<TheMainMenu/>
 			</slot>
 		</aside>
-		<div class="layout-content">
+		<div class="layout-content col-span-5 bg-stone-800 rounded p-2">
 			<slot/>
 		</div>
 	</section>
@@ -24,25 +26,25 @@ export default {
 
 <style scoped>
 .default-layout-root {
-	display: grid;
-	grid-template-columns: 1fr 5fr;
-	gap: 1rem;
+	//display: grid;
+	//grid-template-columns: 1fr 5fr;
+	//gap: 1rem;
 }
 
 .layout-menu {
 	//background: #414349;
-	background: #313339;
-	border-radius: 4px;
-	padding: 1rem;
+	//background: #313339;
+	//border-radius: 4px;
+	//padding: 1rem;
 }
 
 .layout-content {
-	background-color: #313339;
-	//background-image:
-	//	linear-gradient(#414349 1px, transparent 1px),
-	//	linear-gradient(90deg, #414349 1px, transparent 1px);
-	//background-size: 2vw 2vw;
-	border-radius: 4px;
-	padding: 1rem;
+	//background-color: #313339;
+	////background-image:
+	////	linear-gradient(#414349 1px, transparent 1px),
+	////	linear-gradient(90deg, #414349 1px, transparent 1px);
+	////background-size: 2vw 2vw;
+	//border-radius: 4px;
+	//padding: 1rem;
 }
 </style>
