045f6846b74d75b1c1ea0dcf514b53c24771de33
Matteo Porrani <m.porrani@gmail.com>
2025-03-22 11:37:18 +0100

    refacto of layout

--- src/components/layout/DefaultLayout.vue
+++ src/components/layout/DefaultLayout.vue
@@ -1,8 +1,8 @@
 <template>
-	<section
-		class="default-layout-root grid grid-cols-6 gap-2"
-	>
+	<section class="default-layout-root grid grid-cols-6 gap-2">
 		<aside class="layout-menu bg-stone-800 rounded p-2">
+			<h1 class="text-2xl font-bold">Beta-Box</h1>
+			<div class="h-10"></div>
 			<slot name="menu">
 				<TheMainMenu/>
 			</slot>
@@ -22,29 +22,3 @@ export default {
 	components: { TheMainMenu },
 }
 </script>
-
-
-<style scoped>
-.default-layout-root {
-	//display: grid;
-	//grid-template-columns: 1fr 5fr;
-	//gap: 1rem;
-}
-
-.layout-menu {
-	//background: #414349;
-	//background: #313339;
-	//border-radius: 4px;
-	//padding: 1rem;
-}
-
-.layout-content {
-	//background-color: #313339;
-	////background-image:
-	////	linear-gradient(#414349 1px, transparent 1px),
-	////	linear-gradient(90deg, #414349 1px, transparent 1px);
-	////background-size: 2vw 2vw;
-	//border-radius: 4px;
-	//padding: 1rem;
-}
-</style>
--- src/components/layout/TheMainMenu.vue
+++ src/components/layout/TheMainMenu.vue
@@ -1,11 +1,10 @@
 <template>
-	<div>
-		<h1>B-Box</h1>
-
+	<div class="the-main-menu-root flex flex-col gap-4">
 		<router-link
 			v-for="l in links"
 			:key="l[0]"
 			:to="l[1]"
+			class="hover:text-emerald-400"
 		>{{ l[0] }}</router-link>
 	</div>
 </template>
@@ -36,17 +35,5 @@ export default {
 
 
 <style scoped>
-div {
-	display: flex;
-	flex-direction: column;
-	gap: 8px;
-}
 
-h1 {
-	font-size: 1.75rem;
-	display: flex;
-	align-items: center;
-	gap: 4px;
-	margin-bottom: 4rem;
-}
 </style>
