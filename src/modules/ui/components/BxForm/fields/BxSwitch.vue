f97f45090db709a6dac1adbd00cf817216a80a3b
Matteo Porrani <m.porrani@gmail.com>
2025-04-27 13:28:30 +0200

    fix checkbox color

--- src/modules/ui/components/BxForm/fields/BxSwitch.vue
+++ src/modules/ui/components/BxForm/fields/BxSwitch.vue
@@ -14,7 +14,7 @@
 				peer-checked:after:border-white
 				after:content-[''] after:absolute after:top-[2px] after:start-[2px]
 				after:bg-stone-300 after:rounded-full
-				after:size-4 after:transition-all peer-checked:bg-lime-600
+				after:size-4 after:transition-all peer-checked:bg-sky-500
 			"
 		/>
 	</label>
