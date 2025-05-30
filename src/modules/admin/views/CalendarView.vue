b69e0f7a7b81b79005bf8eacbccd931f17f37820
Matteo Porrani <m.porrani@gmail.com>
2025-05-30 12:37:36 +0200

    move Luxon logic to CalendarView

--- src/modules/admin/views/CalendarView.vue
+++ src/modules/admin/views/CalendarView.vue
@@ -12,10 +12,75 @@
 
 
 <script setup>
+import moment from "moment/moment";
+import { DateTime } from "luxon";
 
 import DefaultLayout from "@/modules/core/components/layout/DefaultLayout.vue";
 
 
+
+function testLuxon() {
+
+	const start = DateTime.fromISO("2024-04-21");
+	const ref = DateTime.fromISO("2024-04-21");
+
+
+	console.log("toISO", start.toISO())
+	console.log("toISODate", start.toISODate())
+
+	console.log(start < ref);
+	console.log(start <= ref);
+	console.log(start.toMillis() === ref.toMillis());
+
+	const startDay = DateTime.fromISO("2025-07-01");
+	console.log(startDay.daysInMonth);
+
+	[1, 2, 3].forEach(it => {
+		console.log("+",it)
+		const next = startDay.plus({ days: it })
+		console.log(next.toLocaleString())
+	})
+
+
+	/*
+	console.log(new Date("2025-01-01").toString()) 					// considered UTC 	=> Wed Jan 01 2025 ❌ 01:00:00 GMT+0100 (heure normale d’Europe centrale)
+	console.log(new Date("2025-01-01T00:00:00").toString()) // considered LOCAL => Wed Jan 01 2025 ✅ 00:00:00 GMT+0100 (heure normale d’Europe centrale)
+	*/
+
+	console.log(
+		// DateTime.fromISO("2025-01-01").toString()
+		DateTime.fromISO("2025-01-01").toLocaleString(DateTime.DATETIME_HUGE)
+	)
+
+	console.log(
+		// DateTime.fromISO("2025-01-01T00:00:00").toString()
+		DateTime.fromISO("2025-03-31").toLocaleString(DateTime.DATETIME_HUGE)
+	)
+
+	console.log(
+		DateTime.fromISO("2025-01-01T00:00:00+01:00").toString()
+	)
+
+	console.log(
+		DateTime.fromISO("2025-01-01T00:00:00Z").toString()
+	)
+
+}
+
+
+function compareDateAndMoment() {
+	console.log(new Date("2025-01-01T00:00:00"))  // Wed Jan 01 2025 00:00:00 GMT+0100 (heure normale d’Europe centrale)
+
+	console.log(new Date("2025-01-01")) 					// Wed Jan 01 2025 01:00:00 GMT+0100 (heure normale d’Europe centrale)
+	console.log(new Date("2025-01-01T00:00:00Z")) // Wed Jan 01 2025 01:00:00 GMT+0100 (heure normale d’Europe centrale)
+
+	console.log(new Date("2025-05-31"))						// Sat May 31 2025 02:00:00 GMT+0200 (heure d’été d’Europe centrale)
+
+	console.log(moment("2025-01-01T00:00:00"))		// Wed Jan 01 2025 00:00:00 GMT+0100 (heure normale d’Europe centrale)
+	console.log(moment("2025-01-01"))							// Wed Jan 01 2025 00:00:00 GMT+0100 (heure normale d’Europe centrale)
+	console.log(moment("2025-01-01T00:00:00Z"))   // Wed Jan 01 2025 01:00:00 GMT+0100 (heure normale d’Europe centrale)
+	console.log(moment("2025-05-31"))   					// Sat May 31 2025 00:00:00 GMT+0200 (heure d’été d’Europe centrale)
+}
 
 </script>
 
